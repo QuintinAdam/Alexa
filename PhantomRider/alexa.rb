7caafc0372c10be3c849e5ca581e2306e76456d0
Quintin Adam <quintinadam@live.com>
2015-07-13 22:25:05 -0600

    moar insult fix

--- PhantomRider/alexa.rb
+++ PhantomRider/alexa.rb
@@ -7,39 +7,19 @@ require './insult'
 require 'httparty'
 require 'ffaker'
 
-# We must return application/json as our content type.
 before do
   content_type('application/json')
 end
 
-#enable :sessions
 post '/' do
-  # Check that it's a valid Alexa request
   request_json = JSON.parse(request.body.read.to_s)
-  # Creates a new Request object with the request parameter.
   request = AlexaRubykit.build_request(request_json)
 
-  # We can capture Session details inside of request.
-  # See session object for more information.
   session = request.session
-  p session.new?
-  p session.has_attributes?
-  p session.session_id
-  p session.user_defined?
 
-  # We need a response object to respond to the Alexa.
   response = AlexaRubykit::Response.new
 
-  # We can manipulate the request object.
-  #
-  #p "#{request.to_s}"
-  #p "#{request.request_id}"
-
-  # Response
-  # If it's a launch request
   if (request.type == 'LAUNCH_REQUEST')
-    # Process your Launch Request
-    # Call your methods for your application here that process your Launch Request.
     response.add_speech('Phantom Rider is running!')
     response.add_hash_card( { title: 'Phantom Rider Run', subtitle: 'Phantom Rider Running!' } )
   end
@@ -57,18 +37,16 @@ post '/' do
       response.add_hash_card( { title: persons_name, subtitle: message['motivation'] } )
     elsif request.name == "PhilsosophyIntent"
       response.add_speach("#{Philosophy.new.get_quote.sample}")
-    else #help intent?
+    else
       response.add_speach("I do not want to help you!")
     end
   end
 
   if (request.type =='SESSION_ENDED_REQUEST')
-    # Wrap up whatever we need to do.
     p "#{request.type}"
     p "#{request.reason}"
     halt 200
   end
 
-  # Return response
   response.build_response
 end
--- PhantomRider/insult.rb
+++ PhantomRider/insult.rb
@@ -3,23 +3,4 @@ class Insult
   def initialize
   end
 
-  def get_quote
-    [
-      'If you love something, set it free. Unless it\'s a tiger.',
-      'When life gives you lemonade, make lemons.  Life will be all like, "What?!?!"',
-      'You can tell a lot about a person from his biography.',
-      'Success is 1% inspiration, 98% perspiration, and 2% attention to detail.',
-      'Always look people in the eye, even if they\'re blind.  Just say, "I\'m looking you in the eye."',
-      'If you get pulled over for speeding, tell the policeman your spouse has diarrhea.',
-      'You only get one chance at a first impression. I suggest Julia Child because it's easy to do.',
-      'The most amazing things that can happen to a human being will happen to you if you just lower your expectations.',
-      'Dance until your feet hurt. Sing until your lungs hurt. Act until you're William Hurt.',
-      'Take a lesson from parakeets: If you're ever feeling lonely, eat in a front of a mirror.',
-      'Never be afraid to reach for the stars because even if you fall, you'll always be wearing a Parentchute™.',
-      'Older black ladies make the best iced tea.',
-      'Watch a sunrise at least once a day.',
-      'If you're ever in a jam, a crayon scrunched up under your nose makes a good pretend mustache.'
-    ]
-  end
-
 end
