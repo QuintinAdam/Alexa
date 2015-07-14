600f27911bef7e3c6110e187ce2fb4d65c4753ca
Quintin Adam <quintinadam@live.com>
2015-07-13 22:16:26 -0600

    pushed

--- PhantomRider/philosophy.rb
+++ PhantomRider/philosophy.rb
@@ -11,14 +11,14 @@ class Philosophy
       'Success is 1% inspiration, 98% perspiration, and 2% attention to detail.',
       'Always look people in the eye, even if they\'re blind.  Just say, "I\'m looking you in the eye."',
       'If you get pulled over for speeding, tell the policeman your spouse has diarrhea.',
-      'You only get one chance at a first impression. I suggest Julia Child because it's easy to do.',
+      'You only get one chance at a first impression. I suggest Julia Child because it\'s easy to do.',
       'The most amazing things that can happen to a human being will happen to you if you just lower your expectations.',
-      'Dance until your feet hurt. Sing until your lungs hurt. Act until you're William Hurt.',
-      'Take a lesson from parakeets: If you're ever feeling lonely, eat in a front of a mirror.',
-      'Never be afraid to reach for the stars because even if you fall, you'll always be wearing a Parentchute™.',
+      'Dance until your feet hurt. Sing until your lungs hurt. Act until you\'re William Hurt.',
+      'Take a lesson from parakeets: If you\'re ever feeling lonely, eat in a front of a mirror.',
+      'Never be afraid to reach for the stars because even if you fall, you\'ll always be wearing a Parentchute.',
       'Older black ladies make the best iced tea.',
       'Watch a sunrise at least once a day.',
-      'If you're ever in a jam, a crayon scrunched up under your nose makes a good pretend mustache.'
+      'If you\'re ever in a jam, a crayon scrunched up under your nose makes a good pretend mustache.'
     ]
   end
 
